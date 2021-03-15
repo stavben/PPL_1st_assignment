904b9ad862ecba3d7f8b49a83358bc1b78af5ae7
stavben <77573447+stavben@users.noreply.github.com>
2021-03-15 15:40:57 +0200

    Update part2.ts

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -8,6 +8,21 @@ const countVowels = (str: string): number => {
 
 console.log(countVowels("This is SOME Text"));
 
+//#2
+
+const rec : (str : string[], last : string, current : number, counter : number, output :string )=> string = (str,last,current,counter,output) =>{
+    if (current==str.length+1)
+    return output;
+    else if (last== str[current])
+    return rec(str, last, ++current,++counter,output);
+    else return rec(str, str[current], ++current, 1,output.concat(last+counter));
+}
+
+export const runLengthEncoding : (str : string)=> string = str =>{
+    let arr : string[] = stringToArray(str); 
+    return rec(arr,arr[0],0,0,"");
+};
+
 //#3
 let isBalancedParenthesis = (str: string, parenthesesLeft: string, parenthesesRight: string,) => {
     let wrongArrangement = false;
